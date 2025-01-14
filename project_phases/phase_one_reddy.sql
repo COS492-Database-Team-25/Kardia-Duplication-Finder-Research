884c6ee854e36afcc196a2aea39f0b4361f92200
Colten Yeats <coltenyeats@gmail.com>
2025-01-14 18:38:25 -0500

    fixing base code

--- project_phases/phase_one_reddy.sql
+++ project_phases/phase_one_reddy.sql
@@ -1,7 +1,7 @@
 SELECT 
-    p_partner_key as id, p_given_name as given_name, p_surname as surname, 
+    p_partner_key as id, p_given_name as given_name, p_surname as surname 
 FROM
-    Kardia_DB.p_partner, Kardia_DB.p
+    Kardia_DB.p_partner,
 WHERE
     (p_given_name , p_surname) IN (SELECT 
             p_given_name as given_name, p_surname as surname
