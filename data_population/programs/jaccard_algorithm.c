3b3dce243a7a5aadc9a033912b95d69dde74de28
Colten Yeats <coltenyeats@gmail.com>
2025-01-21 16:57:18 -0500

    jaccard now working

--- data_population/programs/jaccard_algorithm.c
+++ data_population/programs/jaccard_algorithm.c
@@ -1,73 +1,140 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
-int ele_pre(char* S[], char chr)
+int ele_pre(const char* S, char chr)
 {
-    int n, i;
-    n = strlen(S[0]);
-    for (i = 0; S[i] != '\0'; i++)
+    for (int i = 0; S[i] != '\0'; i++)
         if (S[i] == chr)
-            return 1;
-    return 0;
+            return 1; // Character was found
+    return 0; // Character was not found
 }
 
-char* set_union(const char* S1[], const char* S2[])
+char* set_union(const char* S1, const char* S2)
 {
-    char* str_union = (char*)malloc(200 * sizeof(char)); // Allocate memory for the union
+    // Allocate memory where necessary
+    size_t max_size = strlen(S1) + strlen(S2) + 1;
+    char* str_union = (char*)malloc(max_size * sizeof(char));
     if (!str_union) {
-        perror("Failed to allocate memory");
+        perror("Failed to allocate memory, set_union\n");
         exit(1);
     }
 
-    // Copy first string into union string
-    strcpy(str_union, S1);
-    printf(str_union);
+    size_t pos = 0;
 
-    int n = strlen(S1);
-    for (int i = 0; i <= n; i++)
-        str_union[i] = *S1[i];
+    // Copy first string into union string (only alphanumerical)
+    for (int i = 0; S1[i] != '\0'; i++)
+        if (isalnum(S1[i]))
+            str_union[pos++] = S1[i];
 
-    str_union[n] = '\0'; //Signifies the end of the string
+    // Add characters from second string (only alphanumerical)
+    for (int i = 0; S2[i] != '\0'; i++)
+        if (isalnum(S2[i]) && !ele_pre(str_union, S2[i]))
+            str_union[pos++] = S2[i];
+
+    str_union[pos] = '\0'; // End string with null
+
+    printf("In Union: %s\n", str_union);
+
     return str_union;
 }
 
-char* intersection(char* S1[], char* S2[])
+// Works best without duplicate letters
+char* intersection(char* S1, char* S2)
 {
-    char* str_intersect = (char*)malloc(100 * sizeof(char)); // Allocate memory for the intersection
+    size_t max_size = strlen(S1)+strlen(S2);
+    // Allocate memory necessary
+    char* str_intersect = (char*)malloc(max_size * sizeof(char));
     if (!str_intersect) {
-        perror("Failed to allocate memory");
+        perror("Failed to allocate memory, str_intersection\n");
         exit(1);
     }
 
-    int j = 0;
+    size_t pos = 0;
+    for (int i = 0; S1[i] != '\0'; i++)
+        /* if S1[i] is found in S2 and is NOT in str_intersect */
+        if (ele_pre(S2, S1[i])) {
+            // Make max_size larger if necessary
+            if (pos + 1 >= max_size) {
+                max_size *= 2;
+                char* new_intersect = (char*)realloc(str_intersect, max_size); // Allocate memory for the intersection
+                if (!new_intersect) {
+                    perror("Failed to reallocate memory, str_intersection\n");
+                    free(str_intersect);
+                    exit(1);
+                }
+                str_intersect = new_intersect;
+            }
+            str_intersect[pos++] = S1[i];
+        }
 
-    for (int i = 1; S1[i] != '\0'; i++)
-        if (ele_pre(S2, *S1[i]))
-            str_intersect[++j] = *S1[i];
-
-    str_intersect[j] = '\0';
+    str_intersect[pos] = '\0'; //End string with null
+    printf("In Intersect: %s\n", str_intersect);
     return str_intersect;
 }
 
-double jaccard_sim(char* S1[], char* S2[])
+// Takes more time, but handles duplicate letters better
+char* intersection_multiset(char* S1, char* S2)
 {
-    char* str_intersect = intersection(S1, S2);
+    // Allocate memory for str_int_mult
+    size_t max_size = strlen(S1) + strlen(S2);
+    char* str_int_mult = (char*)malloc(max_size * sizeof(char));
+    if (!str_int_mult) {
+        perror("Failed to allocate memory, str_int_mult\n");
+        exit(1);
+    }
+
+    size_t pos = 0;
+    int char_count_S2[256] = {0};
+
+    for (int i = 0; S2[i] != '\0'; i++) {
+        if (isalnum(S2[i])) { // Only count alphanumeric characters
+            char_count_S2[(unsigned char)S2[i]]++;
+        }
+    }
+
+    // Add characters from S1 that are in S2 (up to their count in S2)
+    for (int i = 0; S1[i] != '\0'; i++) {
+        if (isalnum(S1[i]) && char_count_S2[(unsigned char)S1[i]] > 0) {
+            if (pos + 1 >= max_size) {
+                max_size *= 2;
+                char* new_intersect = (char*)realloc(str_int_mult, max_size);
+                if (!new_intersect) {
+                    perror("Failed to reallocate memory, str_int_mult\n");
+                    free(str_int_mult);
+                    exit(1);
+                }
+                str_int_mult = new_intersect;
+            }
+            str_int_mult[pos++] = S1[i];
+            char_count_S2[(unsigned char)S1[i]]--; // Decrement count for character
+        }
+    }
+
+    str_int_mult[pos] = '\0'; //End string with null
+    printf("In Normalized Intersect: %s\n", str_int_mult);
+    return str_int_mult;
+}
+
+double jaccard_sim(char* S1, char* S2) 
+{
     char* str_union = set_union(S1, S2);
+    char* str_intersect = intersection_multiset(S1, S2);
 
-    double jaccard_val = strlen(str_intersect) / strlen(str_union);
+    double jaccard_val = (double)strlen(str_intersect) / strlen(str_union);
 
-    //Clean variables from memory after use
+    // Clean variables from memory after use
     free(str_intersect);
-    free(str_intersect);
+    free(str_union);
 
     return jaccard_val;
 }
 
 int main() 
 {
-    char str1[] = "Hello World!";
-    char str2[] = "Help Will!";
+    char str1[] = "Jack and Jill";
+    char str2[] = "Jill & Jack";
 
     printf("Fuzziness (Jaccard Similarity): %f\n", jaccard_sim(str1, str2));
     return 0;
