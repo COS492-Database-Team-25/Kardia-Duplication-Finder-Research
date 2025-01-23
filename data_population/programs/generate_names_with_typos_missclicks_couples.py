4d62492c738079b1ef4d64dd736918d390bcdc5b
Caleb Ranzau <cranzau28@gmail.com>
2025-01-23 14:54:38 -0500

    I made the alg_test python script that allows us to test the data there is going to be documentation on how to use it later for now I am going to text you guys in slack

--- data_population/programs/generate_names_with_typos_missclicks_couples.py
+++ data_population/programs/generate_names_with_typos_missclicks_couples.py
@@ -67,6 +67,7 @@ def swap(name, index,left_or_right):
 
 def main():
     data = []
+    data_to_be_saved = []
     number_of_typos = 0
     number_of_couples = 0
     number_of_missclicks = 0
@@ -88,8 +89,11 @@ def main():
         email = create_email(first_name,last_name,random_number2,random_number,couple)
         new_row = {"id":str(x),"given_name":first_name,"preferred_name":first_name,"surname":last_name,"email":email,
                    "address":address,"match":matching,"typo":typo,"couple":couple,"missclick":Is_missclick}
+        new_data = ['id',str(x),first_name,last_name,email,address]
+        data_to_be_saved.append(new_data)
         data.append(new_row)
         x+=1
+        
 
     #matches in first name
     for i in range(5000):
@@ -148,24 +152,28 @@ def main():
         email_typo = create_email(first_name_typo,last_name_typo,random_number2,random_number,couple)
         new_row = {"id":str(x),"given_name":first_name_typo,"preferred_name":first_name_typo,"surname":last_name_typo,"email":email_typo,"address":address,"match":matching,"typo":typo,"couple":couple,
                    "missclick":Is_missclick}
+        new_data = ['id',str(x),first_name_typo,last_name_typo,email,address]
+        data_to_be_saved.append(new_data)
         data.append(new_row)
         #print(str(x)+","+first_name_typo+","+first_name_typo, last_name_typo+","+last_name_typo+","+matching+","+typo)
         x+=1
         new_row = {"id":str(x),"given_name":first_name,"preferred_name":first_name,"surname":last_name,"email":email,"address":address,"match":matching,"typo":typo,"couple":couple,"missclick":Is_missclick}
         data.append(new_row)
+        new_data = ['id',str(x),first_name,last_name,email,address]
+        data_to_be_saved.append(new_data)
         x+=1
 
-
-
+    #print(data)
     print("Number of typos: ", number_of_typos)
     print("Number of couples: ", number_of_couples)
     print("Number of missclicks: ", number_of_missclicks)
+    print(data_to_be_saved)
     with open("test_data_with_couples_and_typos_missclicks_and_emails.csv", mode="w", newline="") as file:
         fieldnames = ["id","given_name","preferred_name","surname","email","address","match","typo","couple","missclick"]
         writer = csv.DictWriter(file, fieldnames=fieldnames)
 
         writer.writeheader()
         writer.writerows(data)
-
+    
     print("Data written to test_data_with_couples_and_typos.csv")
 main()
--- project_phases/cosine/alg_test.py
+++ project_phases/cosine/alg_test.py
@@ -1,4 +1,72 @@
-from ctypes import CDLL
-lib = CDLL("./library.so")
-result = lib.main("cat", "pat")
-print(result)
+from ctypes import CDLL, c_char_p, c_double
+import csv
+import time
+timer = 0
+timer_done = 'no'
+cosine_lib = CDLL('./cosine_similarity.so')
+cosine_lib.cosine_similarity_interface.argtypes = [c_char_p, c_char_p]
+cosine_lib.cosine_similarity_interface.restype = c_double
+fuzzyness = .9
+possablematches = 0
+data = []
+startingdata = []
+idtracker = 0
+idtracker_two = 0
+with open('test_data_with_couples_and_typos_and_missclicks copy.csv', mode='r') as file:
+        reader = csv.reader(file)
+        header = next(reader)
+        for row in reader:
+                #print(row[3])
+                person_one_first = row[1].strip()
+                person_one_last = row[3].strip()
+                person_one = person_one_first + " " + person_one_last
+                #print(person_one)
+                person_one = person_one.encode('utf-8')
+                startingdata.append(person_one)
+        print('finding dups')
+        while timer_done == 'no':
+          time.sleep(1)
+          timer = timer + 1
+          for person_one in startingdata:
+                  #print(person_one)
+                  idtracker += 1
+                  for person_two in data:
+                          idtracker_two += 1
+                          result = cosine_lib.cosine_similarity_interface(person_one, person_two)
+                          if float(result) > fuzzyness and idtracker != idtracker_two:
+                                  possablematches += 1
+                                  new_row = {"id_one":idtracker , "name_one":person_one,"name_two":person_two}
+                                  data.append(new_row)
+                          idtracker_two = 0
+        timer_done = "yes"
+                                        #print(person_one,person_two)
+        print(possablematches)
+        print(timer)
+
+        with open("matches.csv", mode="w", newline="") as file:
+                fieldnames = ["id_one","name_one","id_two","name_two"]
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+                writer.writeheader()
+                writer.writerows(data)
+    
+        print("Data written to matches.csv")
+
+'''
+        for row2 in reader:
+                #print(row2[3])
+                person_two_first = row2[1].strip()
+                person_two_last = row2[3].strip()
+                person_two = person_two_first + " " + person_two_last
+                #print(person_two)
+                person_two = person_two.encode('utf-8')
+                #print(person_two)
+                result = cosine_lib.cosine_similarity_interface(person_one, person_two)
+                if float(result) > fuzzyness and row[0] != row2[0]:
+                        print(row[0]," ",row2[0])
+                        print(result)
+                        possablematches += 1
+                        #print(person_one,person_two)
+            #print(result)
+    print(possablematches)
+'''
