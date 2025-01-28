63cc93c91e62b86969fcaa34b37334d57e6aa050
Caleb Ranzau <cranzau28@gmail.com>
2025-01-28 09:30:06 -0500

    super fast alg_test

--- programs/alg_test.py
+++ programs/alg_test.py
@@ -2,9 +2,7 @@
 
 from ctypes import CDLL, c_char_p, c_double
 import csv
-import time
-timer = 0
-timer_done = 'no'
+import timeit
 
 # Determines algorithm used by code
 while True:
@@ -52,79 +50,63 @@ while True:
                 print("Number not in between 0.0 and 1.0, please input a real number between 0.0 and 1.0")
         else:
                 break
-possablematches = 0
-data = []
-startingdata = []
-idtracker = 0
-idtracker_two = 0
-with open('../data/test_data_30000.csv', mode='r') as file:
-        reader = csv.reader(file)
-        header = next(reader)
-        for row in reader:
-                #print(row[3])
-                person_one_first = row[1].strip()
-                person_one_last = row[3].strip()
-                person_one = person_one_first + " " + person_one_last
-                #print(person_one)
-                #person_one = person_one.encode('utf-8')
-                startingdata.append(person_one)
-        print('finding dups')
-        '''
-        while timer_done == 'no':
-          time.sleep(1)
-          timer = timer + 1
-          if timer % 1000 == 1:
-                  print("Im still alive time: "+str(timer))
-        '''
-        if True:
-          for person_one in startingdata:
-                  idtracker_two = 0
-                  idtracker += 1
-                  #print(person_one)
-                  person_one = person_one.encode('utf-8')
-                  for person_two in startingdata:
-                          #print(person_two)
-                          idtracker_two += 1
-                          person_two = person_two.encode('utf-8')
-                          result = lib_function(person_one, person_two)
-                          if result > fuzzyness and idtracker != idtracker_two:
-                                  print(result)
-                                  possablematches += 1
-                                  new_row = {"id_one":idtracker , "name_one":person_one,"id_two":idtracker_two,"name_two":person_two}
-                                  data.append(new_row)
-                                  #print(new_row)
-                
-                #idtracker = 0
-        print("tee hee")
-        timer_done = "yes"
-                                        #print(person_one,person_two)
-        print(possablematches)
-        print(timer)
 
-        with open("matches.csv", mode="w", newline="") as file:
-                fieldnames = ["id_one","name_one","id_two","name_two"]
-                writer = csv.DictWriter(file, fieldnames=fieldnames)
+while True:
+        data_size = input("Enter a letter for a database size (A:10,000 B:20,000 C:30,000) ->")
+        if data_size == "A" or data_size == "a":
+                size = "10000"
+                break
+        if data_size == "B" or data_size == "b":
+                size = "20000"
+                break
+        if data_size == "C" or data_size == "c":
+                size = "30000"
+                break
+def test(fuzzyness, size):
+        possablematches = 0
+        data = []
+        startingdata = []
+        idtracker = 0
+        idtracker_two = 0
+        with open('../data/test_data_'+size+'.csv', mode='r') as file:
+                reader = csv.reader(file)
+                header = next(reader)
+                for row in reader:
+                        person_one_first = row[1].strip()
+                        person_one_last = row[3].strip()
+                        person_one = person_one_first + " " + person_one_last
+                        startingdata.append(person_one)
+                print('finding dups')
+                for person_one in startingdata:
+                        if idtracker % 100 == 0:
+                                print(idtracker)
+                        idtracker_two = 0
+                        idtracker += 1
+                        person_one = person_one.encode('utf-8')
+                        for person_two in startingdata:
+                                idtracker_two += 1
+                                person_two = person_two.encode('utf-8')
+                                if idtracker < idtracker_two:
+                                        #print(idtracker, idtracker_two)
+                                        result = lib_function(person_one, person_two)
+                                        if result > fuzzyness and idtracker != idtracker_two:
+                                                possablematches += 1
+                                                new_row = {"id_one":idtracker , "name_one":person_one,"id_two":idtracker_two,"name_two":person_two}
+                                                data.append(new_row)
+                                
+                                        
+                print("tee hee")
+                timer_done = "yes"
+                print(possablematches)
 
-                writer.writeheader()
-                writer.writerows(data)
+                with open("matches.csv", mode="w", newline="") as file:
+                        fieldnames = ["id_one","name_one","id_two","name_two"]
+                        writer = csv.DictWriter(file, fieldnames=fieldnames)
+
+                        writer.writeheader()
+                        writer.writerows(data)
     
         print("Data written to matches.csv")
 
-'''
-        for row2 in reader:
-                #print(row2[3])
-                person_two_first = row2[1].strip()
-                person_two_last = row2[3].strip()
-                person_two = person_two_first + " " + person_two_last
-                #print(person_two)
-                person_two = person_two.encode('utf-8')
-                #print(person_two)
-                result = cosine_lib.cosine_similarity_interface(person_one, person_two)
-                if float(result) > fuzzyness and row[0] != row2[0]:
-                        print(row[0]," ",row2[0])
-                        print(result)
-                        possablematches += 1
-                        #print(person_one,person_two)
-            #print(result)
-    print(possablematches)
-'''
+time = timeit.timeit(test(fuzzyness,size))
+print("Time "+temp+" took to run: "+time)
